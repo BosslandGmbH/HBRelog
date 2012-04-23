572ff3e5979c88c29fbb2f41c419f68af2276593
HelloInnovation <github@helloinnovation.com>
2012-04-23 17:39:15 +0000

    HB will now run under windows 8 if started by HBRelog

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2012/04/20 16:45:00
+// Build Date: 2012/04/22 18:35:53
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.79")]
-[assembly: AssemblyFileVersion("1.0.0.79")]
+[assembly: AssemblyVersion("1.0.0.80")]
+[assembly: AssemblyFileVersion("1.0.0.80")]
