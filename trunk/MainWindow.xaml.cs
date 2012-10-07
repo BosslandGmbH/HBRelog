a95cb10183231811c08ed6382a0e0c6e441dac26
HighVoltz82@gmail.com <HighVoltz82@gmail.com@7a075389-e590-0882-5ac6-d3ef9ecf0140>
2012-10-07 11:57:28 +0000

    Fixed the bug where certain settings were not getting saved.

--- trunk/MainWindow.xaml.cs
+++ trunk/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -28,10 +29,12 @@ using HighVoltz.HBRelog.Tasks;
 namespace HighVoltz.HBRelog
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///   Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Timer _autoCloseTimer;
+
         public MainWindow()
         {
             //InitializeComponent();
@@ -43,10 +46,33 @@ namespace HighVoltz.HBRelog
 
         public static MainWindow Instance { get; private set; }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            var saveTimespan = HbRelogManager.Settings.SaveCompleteTimeSpan;
+            if (HbRelogManager.Settings != null && saveTimespan != TimeSpan.FromSeconds(0))
+            {
+                e.Cancel = true;
+                Title = "HBRelog : Waiting for settings to save before exiting";
+                _autoCloseTimer = new Timer(
+                    state =>
+                        {
+                            _autoCloseTimer.Dispose();
+                            _autoCloseTimer = null;
+                            Dispatcher.Invoke((Action)(Close));
+                        },
+                    null,
+                    saveTimespan,
+                    TimeSpan.FromMilliseconds(-1));
+            }
+            else
+                base.OnClosing(e);
+        }
+
+
         public void LoadStyle()
         {
             Uri resourceLocater = HbRelogManager.Settings.UseDarkStyle ? new Uri("/styles/ExpressionDark.xaml", UriKind.Relative) : new Uri("/styles/BureauBlue.xaml", UriKind.Relative);
-            var rDict = new ResourceDictionary { Source = resourceLocater };
+            var rDict = new ResourceDictionary {Source = resourceLocater};
             Resources.MergedDictionaries.Clear();
             Resources.MergedDictionaries.Add(rDict);
         }
@@ -55,7 +81,7 @@ namespace HighVoltz.HBRelog
         {
             var character = new CharacterProfile();
             if (AccountGrid.SelectedItem != null)
-                character.Settings = ((CharacterProfile)AccountGrid.SelectedItem).Settings.ShadowCopy();
+                character.Settings = ((CharacterProfile) AccountGrid.SelectedItem).Settings.ShadowCopy();
 
             if (character.Settings != null)
             {
@@ -68,12 +94,12 @@ namespace HighVoltz.HBRelog
         private void EditAccountButtonClick(object sender, RoutedEventArgs e)
         {
             if (AccountGrid.SelectedItem != null)
-                EditAccount(((CharacterProfile)AccountGrid.SelectedItem).Settings);
+                EditAccount(((CharacterProfile) AccountGrid.SelectedItem).Settings);
         }
 
         private void AcountConfigCloseButtonClick(object sender, RoutedEventArgs e)
         {
-            var ani = new DoubleAnimation(0, new Duration(TimeSpan.Parse("0:0:0.4"))) { AccelerationRatio = 0.7 };
+            var ani = new DoubleAnimation(0, new Duration(TimeSpan.Parse("0:0:0.4"))) {AccelerationRatio = 0.7};
             AccountConfigGrid.BeginAnimation(WidthProperty, ani);
             AccountConfig.IsEditing = false;
             HbRelogManager.Settings.Save();
@@ -83,7 +109,7 @@ namespace HighVoltz.HBRelog
         {
             if (charSettings != null)
             {
-                var ani = new DoubleAnimation(255, new Duration(TimeSpan.Parse("0:0:0.4"))) { DecelerationRatio = 0.7 };
+                var ani = new DoubleAnimation(255, new Duration(TimeSpan.Parse("0:0:0.4"))) {DecelerationRatio = 0.7};
                 AccountConfigGrid.BeginAnimation(WidthProperty, ani);
                 AccountConfig.EditAccount(charSettings);
             }
@@ -91,14 +117,12 @@ namespace HighVoltz.HBRelog
 
         private void DeleteAccountButtonClick(object sender, RoutedEventArgs e)
         {
-            if (AccountGrid.SelectedIndex >= 0 &&
-                MessageBox.Show("Are you sure you want to delete selected profile/s", "Account deletion",
-                                MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            if (AccountGrid.SelectedIndex >= 0 && MessageBox.Show("Are you sure you want to delete selected profile/s", "Account deletion", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
             {
                 // delete all the selected profiles from the data source. 
                 for (int i = AccountGrid.SelectedItems.Count - 1; i >= 0; i--)
                 {
-                    var character = (CharacterProfile)AccountGrid.SelectedItems[i];
+                    var character = (CharacterProfile) AccountGrid.SelectedItems[i];
                     HbRelogManager.Settings.CharacterProfiles.Remove(character);
                 }
                 HbRelogManager.Settings.Save();
@@ -149,17 +173,16 @@ namespace HighVoltz.HBRelog
 
         private void AccountGridSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (AccountConfig != null && AccountGrid != null &&
-                AccountConfig.IsEditing && AccountGrid.SelectedItem != null)
+            if (AccountConfig != null && AccountGrid != null && AccountConfig.IsEditing && AccountGrid.SelectedItem != null)
             {
-                EditAccount(((CharacterProfile)AccountGrid.SelectedItem).Settings);
+                EditAccount(((CharacterProfile) AccountGrid.SelectedItem).Settings);
             }
         }
 
         private void AccountGridMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (AccountGrid.SelectedItem != null)
-                EditAccount(((CharacterProfile)AccountGrid.SelectedItem).Settings);
+                EditAccount(((CharacterProfile) AccountGrid.SelectedItem).Settings);
         }
 
         private void PauseButtonClick(object sender, RoutedEventArgs e)
@@ -212,20 +235,20 @@ namespace HighVoltz.HBRelog
 
         private void ProfileEnabledCheckBoxChecked(object sender, RoutedEventArgs e)
         {
-            var settings = (ProfileSettings)((CheckBox)sender).Tag;
+            var settings = (ProfileSettings) ((CheckBox) sender).Tag;
             settings.IsEnabled = true;
         }
 
         private void ProfileEnabledCheckBoxUnchecked(object sender, RoutedEventArgs e)
         {
-            var settings = (ProfileSettings)((CheckBox)sender).Tag;
+            var settings = (ProfileSettings) ((CheckBox) sender).Tag;
             settings.IsEnabled = false;
         }
 
         // Options And Log Tab Control
         private void TabItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            var tabItem = (TabItem)sender;
+            var tabItem = (TabItem) sender;
             if (e.OriginalSource != tabItem.Header)
                 return;
             double destHeight = 200;
@@ -239,13 +262,13 @@ namespace HighVoltz.HBRelog
                 OptionsAndLogTabCtrl.SelectedItem = tabItem;
             }
             e.Handled = true;
-            var ani = new DoubleAnimation(destHeight, new Duration(TimeSpan.Parse("0:0:0.300"))) { DecelerationRatio = 0.7 };
+            var ani = new DoubleAnimation(destHeight, new Duration(TimeSpan.Parse("0:0:0.300"))) {DecelerationRatio = 0.7};
             OptionsAndLogGrid.BeginAnimation(HeightProperty, ani);
         }
 
         private void SkipTaskMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var profile = (CharacterProfile)((MenuItem)sender).Tag;
+            var profile = (CharacterProfile) ((MenuItem) sender).Tag;
             BMTask currentTask = profile.TaskManager.Tasks.FirstOrDefault(t => !t.IsDone);
             if (currentTask != null)
             {
@@ -255,7 +278,7 @@ namespace HighVoltz.HBRelog
 
         private void BringHbToForegroundMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var hbManager = ((CharacterProfile)((MenuItem)sender).Tag).TaskManager.HonorbuddyManager;
+            var hbManager = ((CharacterProfile) ((MenuItem) sender).Tag).TaskManager.HonorbuddyManager;
             if (hbManager.IsRunning && !hbManager.BotProcess.HasExited)
             {
                 NativeMethods.ShowWindow(hbManager.BotProcess.MainWindowHandle, NativeMethods.ShowWindowCommands.Restore);
@@ -265,7 +288,7 @@ namespace HighVoltz.HBRelog
 
         private void MaximizeWowMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var wowManager = ((CharacterProfile)((MenuItem)sender).Tag).TaskManager.WowManager;
+            var wowManager = ((CharacterProfile) ((MenuItem) sender).Tag).TaskManager.WowManager;
             if (wowManager.IsRunning && !wowManager.GameProcess.HasExited)
             {
                 NativeMethods.ShowWindow(wowManager.GameProcess.MainWindowHandle, NativeMethods.ShowWindowCommands.Maximize);
@@ -275,13 +298,13 @@ namespace HighVoltz.HBRelog
 
         private void DataGridRowContextMenuOpening(object sender, ContextMenuEventArgs e)
         {
-            var row = (DataGridRow)sender;
-            var profile = (CharacterProfile)row.Item;
+            var row = (DataGridRow) sender;
+            var profile = (CharacterProfile) row.Item;
             var hbManager = profile.TaskManager.HonorbuddyManager;
             var wowManager = profile.TaskManager.WowManager;
 
             // only show the skip task menu item if profile has > 1 task and startup sequence is complete
-            var skipTaskMenuItem = (MenuItem)row.ContextMenu.Items[0];
+            var skipTaskMenuItem = (MenuItem) row.ContextMenu.Items[0];
             BMTask task = profile.TaskManager.Tasks.FirstOrDefault(t => t.IsRunning);
             if (profile.TaskManager.Tasks.Count > 1 && profile.TaskManager.StartupSequenceIsComplete && task != null)
                 skipTaskMenuItem.Visibility = Visibility.Visible;
@@ -289,20 +312,18 @@ namespace HighVoltz.HBRelog
                 skipTaskMenuItem.Visibility = Visibility.Collapsed;
 
             //only show the 'Maximize Wow' task menu item if wow is running.
-            var maximizeWowMenuItem = (MenuItem)row.ContextMenu.Items[1];
+            var maximizeWowMenuItem = (MenuItem) row.ContextMenu.Items[1];
             if (wowManager.IsRunning && !wowManager.GameProcess.HasExited)
                 maximizeWowMenuItem.Visibility = Visibility.Visible;
             else
                 maximizeWowMenuItem.Visibility = Visibility.Collapsed;
 
             //only show the 'Bring HB to Foreground' task menu item if hb is running.
-            var bringHbToForegroundTaskMenuItem = (MenuItem)row.ContextMenu.Items[2];
+            var bringHbToForegroundTaskMenuItem = (MenuItem) row.ContextMenu.Items[2];
             if (hbManager.IsRunning && hbManager.BotProcess != null && !hbManager.BotProcess.HasExited)
                 bringHbToForegroundTaskMenuItem.Visibility = Visibility.Visible;
             else
                 bringHbToForegroundTaskMenuItem.Visibility = Visibility.Collapsed;
-           
         }
-
     }
 }
--- trunk/Settings/ProfileSettings.cs
+++ trunk/Settings/ProfileSettings.cs
@@ -70,7 +70,7 @@ namespace HighVoltz.HBRelog.Settings
                 PropertyChanged(this, new PropertyChangedEventArgs(name));
             }
             if (HbRelogManager.Settings != null)
-                HbRelogManager.Settings.AutoSave();
+                HbRelogManager.Settings.QueueSave();
         }
 
     }
