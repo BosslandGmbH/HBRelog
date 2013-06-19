6c1ce89369a0537ee3cd5f8fdbf6287165e78399
HighVoltz82@gmail.com <HighVoltz82@gmail.com@7a075389-e590-0882-5ac6-d3ef9ecf0140>
2013-06-19 16:32:48 +0000

    Fixed Logon task.

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2013/06/18 22:39:55
+// Build Date: 2013/06/19 11:44:29
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.141")]
-[assembly: AssemblyFileVersion("1.0.0.141")]
+[assembly: AssemblyVersion("1.0.0.142")]
+[assembly: AssemblyFileVersion("1.0.0.142")]
