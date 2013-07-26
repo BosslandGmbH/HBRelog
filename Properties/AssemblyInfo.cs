673f4f0915c8836c65ea04484a3ef56c77fc62a0
HelloInnovation <github@helloinnovation.com>
2013-07-26 03:49:37 +0000

    - The WoW Start Delay setting now effects all WoW launches regardless of which directory the WoW.exe resides in. Previously it was used to throttle launches from same paths.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2013/07/25 23:00:11
+// Build Date: 2013/07/25 23:32:17
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.164")]
-[assembly: AssemblyFileVersion("1.0.0.164")]
+[assembly: AssemblyVersion("1.0.0.165")]
+[assembly: AssemblyFileVersion("1.0.0.165")]
