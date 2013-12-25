83649fc769b68060e2f5c95aaeb275de675e04a8
HighVoltz82@gmail.com <HighVoltz82@gmail.com@7a075389-e590-0882-5ac6-d3ef9ecf0140>
2013-12-25 20:10:37 +0000

    Launcher now uses 'CreateProcess' Win32 api

--- trunk/Launcher/Helpers.cs
+++ trunk/Launcher/Helpers.cs
@@ -9,19 +9,6 @@ namespace HighVoltz.Launcher
 {
 	public class Helpers
 	{
-		[Flags]
-		public enum ThreadAccess : int
-		{
-			TERMINATE = (0x0001),
-			SUSPEND_RESUME = (0x0002),
-			GET_CONTEXT = (0x0008),
-			SET_CONTEXT = (0x0010),
-			SET_INFORMATION = (0x0020),
-			QUERY_INFORMATION = (0x0040),
-			SET_THREAD_TOKEN = (0x0080),
-			IMPERSONATE = (0x0100),
-			DIRECT_IMPERSONATION = (0x0200)
-		}
 
 		[DllImport("kernel32.dll")]
 		static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
@@ -29,6 +16,39 @@ namespace HighVoltz.Launcher
 		static extern uint SuspendThread(IntPtr hThread);
 		[DllImport("kernel32.dll")]
 		static extern int ResumeThread(IntPtr hThread);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		static extern bool CreateProcess(string lpApplicationName,
+		   string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes,
+		   ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles,
+		   uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory,
+		   [In] ref STARTUPINFO lpStartupInfo,
+		   out PROCESS_INFORMATION lpProcessInformation);
+
+		public static int? StartProcessSuspended(string fileName, string args)
+		{
+			const uint CREATE_SUSPENDED = 0x00000004;
+
+			var pInfo = new PROCESS_INFORMATION();
+			var sInfo = new STARTUPINFO();
+			var pSec = new SECURITY_ATTRIBUTES();
+			var tSec = new SECURITY_ATTRIBUTES();
+			pSec.nLength = Marshal.SizeOf(pSec);
+			tSec.nLength = Marshal.SizeOf(tSec);
+
+			var result = CreateProcess(
+				null,
+				fileName + " " + args,
+				ref pSec,
+				ref tSec,
+				false,
+				CREATE_SUSPENDED,
+				IntPtr.Zero,
+				null,
+				ref sInfo,
+				out pInfo);
+			return result ? (int?)pInfo.dwProcessId : null;
+		}
 
 		public static void SuspendProcess(int pid)
 		{
@@ -68,6 +88,60 @@ namespace HighVoltz.Launcher
 
 				ResumeThread(pOpenThread);
 			}
+		}
+
+		[Flags]
+		public enum ThreadAccess : int
+		{
+			TERMINATE = (0x0001),
+			SUSPEND_RESUME = (0x0002),
+			GET_CONTEXT = (0x0008),
+			SET_CONTEXT = (0x0010),
+			SET_INFORMATION = (0x0020),
+			QUERY_INFORMATION = (0x0040),
+			SET_THREAD_TOKEN = (0x0080),
+			IMPERSONATE = (0x0100),
+			DIRECT_IMPERSONATION = (0x0200)
+		}
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+		struct STARTUPINFO
+		{
+			public Int32 cb;
+			public string lpReserved;
+			public string lpDesktop;
+			public string lpTitle;
+			public Int32 dwX;
+			public Int32 dwY;
+			public Int32 dwXSize;
+			public Int32 dwYSize;
+			public Int32 dwXCountChars;
+			public Int32 dwYCountChars;
+			public Int32 dwFillAttribute;
+			public Int32 dwFlags;
+			public Int16 wShowWindow;
+			public Int16 cbReserved2;
+			public IntPtr lpReserved2;
+			public IntPtr hStdInput;
+			public IntPtr hStdOutput;
+			public IntPtr hStdError;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct SECURITY_ATTRIBUTES
+		{
+			public int nLength;
+			public unsafe byte* lpSecurityDescriptor;
+			public int bInheritHandle;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct PROCESS_INFORMATION
+		{
+			public IntPtr hProcess;
+			public IntPtr hThread;
+			public int dwProcessId;
+			public int dwThreadId;
 		}
 	}
 }
--- trunk/Launcher/Program.cs
+++ trunk/Launcher/Program.cs
@@ -11,14 +11,17 @@ namespace HighVoltz.Launcher
 	static class Program
 	{
 
-		static void Main(params string[] args)
+		static int Main(params string[] args)
 		{
 			if (args.Length < 1)
-				throw new ArgumentException("You must provide a path to a program to launch", "args");
+			{
+				Console.WriteLine("You must provide a path to a program to launch");
+				return -1;
+			}
 			string programPath = args[0];
 			string arg = args.Length > 1 ? args[1] : "";
-			var proc = Process.Start(programPath, arg);
-			Helpers.SuspendProcess(proc.Id);
+			Helpers.StartProcessSuspended(programPath, arg);
+			return 0;
 		}
 
 	}
