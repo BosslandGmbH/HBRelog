9fbbfe523ea611647d7600a78c9ff3527325c222
HighVoltz82@gmail.com <HighVoltz82@gmail.com@7a075389-e590-0882-5ac6-d3ef9ecf0140>
2013-12-27 01:34:49 +0000

    Fixed a compiler error in the HBRelogHelper plugin.

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2013/12/25 15:10:37
+// Build Date: 2013/12/26 19:39:36
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.195")]
-[assembly: AssemblyFileVersion("1.0.0.195")]
+[assembly: AssemblyVersion("1.0.0.196")]
+[assembly: AssemblyFileVersion("1.0.0.196")]
