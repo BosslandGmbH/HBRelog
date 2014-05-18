edc140aad2acae1a2f48ee4f330603a86a668c5f
HighVoltz82@gmail.com <HighVoltz82@gmail.com@7a075389-e590-0882-5ac6-d3ef9ecf0140>
2014-05-18 05:19:31 +0000

    Upgraded to .net 4.5.1 The HBRelogHelper plugin no-longer spams log with bunch of errors if there is a problem connecting to HBRelog.

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2014/03/30 23:36:39
+// Build Date: 2014/03/31 19:44:09
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.202")]
-[assembly: AssemblyFileVersion("1.0.0.202")]
+[assembly: AssemblyVersion("1.0.0.203")]
+[assembly: AssemblyFileVersion("1.0.0.203")]
