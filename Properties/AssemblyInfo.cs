311e1599f10e745b6907376623b5893cab046da8
HelloInnovation <github@helloinnovation.com>
2014-06-02 17:46:06 +0000

    Added an option to disable setting Window title and also one to change how the title is displayed. See the options tab and tooltips for more details.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2014/03/31 19:44:09
+// Build Date: 2014/05/18 01:19:31
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.203")]
-[assembly: AssemblyFileVersion("1.0.0.203")]
+[assembly: AssemblyVersion("1.0.0.204")]
+[assembly: AssemblyFileVersion("1.0.0.204")]
