0aa5d4401b670da51a3aa19923786496069cec60
HelloInnovation <github@helloinnovation.com>
2015-01-26 03:59:51 +0000

    * Only the first 16 characters of password is now used. * Fasm.NET(https://github.com/ZenLulz/Fasm.NET) is now used in instead of fasmdll_managed.dll

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template AssemblyInfo.template
-// Build Date: 2014/12/30 02:52:10
+// Build Date: 2015/01/05 17:55:38
 // SVN url: https://hbrelog.googlecode.com/svn/trunk/Properties
 
 using System.Reflection;
@@ -55,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.218")]
-[assembly: AssemblyFileVersion("1.0.0.218")]
+[assembly: AssemblyVersion("1.0.0.219")]
+[assembly: AssemblyFileVersion("1.0.0.219")]
