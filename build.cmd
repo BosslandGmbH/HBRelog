665516dd57674c5da90d318a43eec0e871ee70ad
aash <shpitalyov.a.alexey@gmail.com>
2015-08-12 15:22:09 +0600

    fix build script

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+@echo off
 set PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;%PATH%
-msbuild HBRelog.csproj /t:NugetRestore;Build /p:Configuration=Release /p:AllowUnsafeItems=true /p:ReferencePath=%HB_HOME%
+set /p HB_HOME="enter Honorbuddy installation path(where honorbuddy.exe is located): "
+msbuild HBRelog.csproj /t:NugetRestore;Rebuild /p:Configuration=Release /p:AllowUnsafeBlocks=true /p:ReferencePath=%HB_HOME%
 pause
