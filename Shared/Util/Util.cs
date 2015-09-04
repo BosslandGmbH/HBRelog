51f8b9d7e34c7c34c0052ad8ed5d163687d0ea92
aash <shpitalyov.a.alexey@gmail.com>
2015-09-04 13:57:43 +0600

    add admin check

---
+++ Shared/Util/Util.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Security.Principal;
+
+namespace Shared.Util
+{
+    public static class Util
+    {
+        public static bool IsUserAdministrator()
+        {
+            //bool value to hold our return value
+            bool isAdmin;
+            try
+            {
+                //get the currently logged in user
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                isAdmin = false;
+            }
+            catch (Exception)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+    }
+}
