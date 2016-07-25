c1b6b2db9349b698e6277153f29f40697e04ee0a
highvoltz <highvoltz82@gmail.com>
2016-07-25 18:49:54 -0400

    Updated offsets to WoW Legion

--- WoW/FrameXml/Offsets.cs
+++ WoW/FrameXml/Offsets.cs
@@ -6,62 +6,64 @@
         {
             public const int GetTypeVtmOffset = 0x20;
             public const int GetFontTypeVtmOffset = 0x1C;
-            public const int NamePtrOffset = 0x1C;
-            public const int FontNamePtrOffset = 0x5C;
+            public const int NamePtrOffset = 0x14;
+			// Needs updating
+            public const int FontNamePtrOffset = 0x54;
         }
 
         public static class ParentedObject
         {
-            public const int ParentOffset = 0x98;
+            public const int ParentOffset = 0x84;
         }
 
         public static class Region
         {
-            public const int UIScaleOffset = 0x80;
-            public const int BottomOffset = 0x68;
-            public const int LeftOffset = 0x6C;
-            public const int TopOffset = 0x70;
-            public const int RightOffset = 0x74;
+			// This is actually 'EffectiveScale'
+			public const int UIScaleOffset = 0x78;
+            public const int BottomOffset = 0x4C;
+            public const int LeftOffset = 0x50;
+            public const int TopOffset = 0x54;
+            public const int RightOffset = 0x58;
         }
 
         public static class VisibleRegion
         {
-            public const int FlagsOffset = 0x64;
-            public const int IsVisibleRShiftAmount = 26;
-            public const int IsShownRShiftAmount = 25;
+            public const int FlagsOffset = 0x80;
+            public const int IsVisibleRShiftAmount = 19;
+            public const int IsShownRShiftAmount = 18;
         }
 
         public static class Frame // Inherits ScriptObject and VisibleFrame
         {
-            public const int ChildrenOffset = 0x1BC; 
-            public const int RegionsSizeOffset = 0x160;  
-            public const int RegionsOffset = 0x168;  
-            public const int IdOffset = 0xC4;
-            public const int LevelOffset = 0xD8;
-            public const int StrataOffset = 0x1E3;  
+            public const int ChildrenOffset = 0x148; 
+            public const int RegionsSizeOffset = 0x128;  
+            public const int RegionsOffset = 0x130;  
+            public const int IdOffset = 0x94;
+            public const int LevelOffset = 0x158;
+            public const int StrataOffset = 0x157;  
         }
 
         public static class Button // inherits Frame
         {
-            public const int FlagsOffset = 0x1F0;  
-            public const int FontStringOffset = 0x1F8;  
-            public const int HighlightTextureOffset = 0x220; 
+            public const int FlagsOffset = 0x170;  
+            public const int FontStringOffset = 0x178;  
+            public const int HighlightTextureOffset = 0x1A0; 
         }
 
         public static class FontString
         {
-            public const int TextOffset = 0xF8;
+            public const int TextOffset = 0xE4;
         }
 
         public static class EditBox // Inherits FontInstance and Frame
         {
-            public const int FlagsOffset = 0x1F8;  
-            public const int AsciiCursorPositionOffset = 0x24C; 
-            public const int TextOffset = 0x20C;  
-            public const int MaxBytesOffset = 0x21C;  
-            public const int MaxLettersOffset = 0x220;  
-            public const int IsEnabledFlagOffset = 0xC8;  
-            public const int IsEnabledBit = 0x400;
+            public const int FlagsOffset = 0x17C;  
+            public const int AsciiCursorPositionOffset = 0x1D8; 
+            public const int TextOffset = 0x190;  
+            public const int MaxBytesOffset = 0x1A0;  
+            public const int MaxLettersOffset = 0x1A4;  
+            public const int IsEnabledFlagOffset = 0x98;  
+            public const int IsEnabledBit = 8;
             public const int IsAutoFocus = 0x1;
             public const int IsMultilineBit = 0x2;
             public const int IsNumericBit = 0x4;
@@ -71,29 +73,31 @@
 
         public static class ScrollFrame // Inherits from Frame
         {
-            public const int ScrollChildOffset = 0x1E8;  
-            public const int HorizontalScrollRangeOffset = 0x1EC; 
-            public const int VerticalScrollRangeOffset = 0x1F0;  
-            public const int HorizontalScrollOffset = 0x1F4;  
-            public const int VerticalScrollOffset = 0x1F8;  
+            public const int ScrollChildOffset = 0x164;  
+            public const int HorizontalScrollRangeOffset = 0x168; 
+            public const int VerticalScrollRangeOffset = 0x16C;
+			public const int HorizontalScrollOffset = 0x170;  
+            public const int VerticalScrollOffset = 0x174;  
         }
 
         public static class Slider // Inherits from Frame
         {
-            public const int IsEnabledFlagOffset = 0xC8;
-            public const int MinValueOffset = 0x1E4;  
-            public const int MaxValueOffset = 0x1E8;  
-            public const int ValueOffset = 0x1EC;  
-            public const int ValueStepOffset = 0x1F0;  
-            public const int ThumbTextureOffset = 0x1F8; 
-            public const int OrientationOffset = 0x1F4;  
-            public const int IsEnabledBit = 0x400;
+            public const int IsEnabledFlagOffset = 0x98;
+            public const int MinValueOffset = 0x160;  
+            public const int MaxValueOffset = 0x164;  
+            public const int ValueOffset = 0x168;  
+            public const int ValueStepOffset = 0x16C;  
+            public const int ThumbTextureOffset = 0x174; 
+            public const int OrientationOffset = 0x170;  
+            public const int IsEnabledBit = 8;
         }
 
         public static class Texture // Inherits From Layered Region
         {
+			// Needs fixing
             public const int TexturePathObjectOffset = 0xC0;
-            public const int TexturePathOffset = 0x18;
+			// Needs fixing
+			public const int TexturePathOffset = 0x18;
         }
 
     }
--- WoW/WowPatterns.cs
+++ WoW/WowPatterns.cs
@@ -6,26 +6,23 @@ namespace HighVoltz.HBRelog.WoW
     {
         public static readonly Pattern GameStatePattern = Pattern.FromTextstyle(
             "GameState",
-            "80 3d ?? ?? ?? ?? ?? 74 ?? 50 b9 ?? ?? ?? ?? e8 ?? ?? ?? ?? 85 c0 74 ?? 8b 40 08 83 f8 02 74 ?? 83 f8 01 75 ?? b0 01 c3 32 c0 c3",
-            new AddModifier(2),
+			"? ? ? ? ? 74 3A 6A 00 E8 ? ? ? ? 59 39 45 10 74 08 6A 00 E8 ? ? ? ? 59 6A 01 E8 ? ? ? ? 59 39 45 0C 74 08",
             new LeaModifier());
 
         // ref - FrameXML:EditBox:HasFocus
         public static readonly Pattern FocusedWidgetPattern = Pattern.FromTextstyle(
-            "FocusedWidget", "3b 05 ?? ?? ?? ?? 0f 94 c1 51 ff 75 08 e8 ?? ?? ?? ?? 33 c0 83 c4 10 40 5d c3", new AddModifier(2), new LeaModifier());
+            "FocusedWidget", "55 8b ec 6a 00 e8 ? ? ? ? 50 ff 75 08 e8 ? ? ? ? 33 c9 3b 05 ? ? ? ? 0f 94 c1 51 ff 75 08 e8 ? ? ? ? 33 c0 83 c4 14 40 5d c3", new AddModifier(23), new LeaModifier());
 
         // ref - Framescript_ExecuteBuffer.
         public static readonly Pattern LuaStatePattern = Pattern.FromTextstyle(
             "LuaState",
-            "8b 35 ?? ?? ?? ?? 33 db 57 3b c3 74 ?? 88 18 ff 75 08 8d 85 dc fe ff ff 68 ?? ?? ?? ?? 68 ?? ?? ?? ?? 50",
-            new AddModifier(2),
+			"? ? ? ? 6a 00 ff 75 0c 56 e8 ? ? ? ? ff 75 08 56 e8 ? ? ? ? 6a fe 56 e8 ? ? ? ? 68 ? ? ? ? 56 e8 ? ? ? ? 83 c4 24 5e 5d c3",
             new LeaModifier());
 
 		// first offset used in 'LoadingScreenEnable' function. This function also fires the 'LOADING_SCREEN_ENABLED' lua event.
 	    public static readonly Pattern LoadingScreenEnableCountPattern = Pattern.FromTextstyle(
 		    "LoadingScreenEnableCount",
-		    "ff 05 ?? ?? ?? ?? 83 3d ?? ?? ?? ?? ?? 53 56 57 0f 8f ?? ?? ?? ?? 6a 00 e8 ?? ?? ?? ?? 59 e8 ?? ?? ?? ?? 84 c0 74 ?? 6a 00 68",
-		    new AddModifier(2),
+			"? ? ? ? 83 EC 18 48 A3 ? ? ? ? 85 C0 0F 8F ? ? ? ? 53 79 18 33 DB 89 1D ? ? ? ? E8 ? ? ? ? 89 1D ? ? ? ? E9",
 		    new LeaModifier());
 
 	    // 
