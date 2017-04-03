c70587a27dfdf0873ca86df270f6b20a5ea61eeb
highvoltz <highvoltz@honorbuddy.com>
2017-04-03 17:59:14 -0400

    Removed some leftover code

--- WoW/FrameXml/ParentedObject.cs
+++ WoW/FrameXml/ParentedObject.cs
@@ -14,9 +14,6 @@ namespace HighVoltz.HBRelog.WoW.FrameXml
             {
                 if (!_triedToGetParent)
                 {
-                    var vft = WowManager.Memory.Read<IntPtr>(Address);
-                    var func = WowManager.Memory.Read<IntPtr>(vft + 32);
-
                     var parentPtr = WowManager.Memory.Read<IntPtr>(Address + Offsets.ParentedObject.ParentOffset);
                     _parent = parentPtr != IntPtr.Zero ? GetUIObjectFromPointer(WowManager, parentPtr) : null;
                     _triedToGetParent = true;
